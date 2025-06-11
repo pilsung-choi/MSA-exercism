e5bffaf916ee031f30ac6e2fe23154f820419afe
pilsung-choi <vlftjd159753@gmail.com>
2025-06-12 07:59:54 +0900

    초기 user

---
+++ apps/user/src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new BadRequestException('이미 가입한 이메일 입니다!');
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await this.userRepository.save({
+      ...createUserDto,
+      email,
+      password: hash,
+    });
+
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}
